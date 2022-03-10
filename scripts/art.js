d9ad5da4ba68470d1756825e0f7593730fb2755e
Zale Davis <zale.davis@gmx.com>
2022-03-10 13:10:43 -0800

    started new site, created structure for index page, art page, music page

---
+++ scripts/art.js
@@ -0,0 +1,39 @@
+const door = document.querySelector('.art-door');
+
+door.hover = function () {
+    albumHolder.classList.toggle('hidden');
+    demosUpHolder.classList.add('hidden');
+    demosDownHolder.classList.add('hidden');
+}
+
+let slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+closeModal();
---
+++ scripts/music.js
@@ -0,0 +1,23 @@
+const album = document.querySelector('.album');
+const albumHolder = document.querySelector('.albumHolder');
+const demosUp = document.querySelector('.demosUp');
+const demosUpHolder = document.querySelector('.demosUpHolder');
+const demosDown = document.querySelector('.demosDown');
+const demosDownHolder = document.querySelector('.demosDownHolder');
+
+
+album.onclick = function () {
+    albumHolder.classList.toggle('hidden');
+    demosUpHolder.classList.add('hidden');
+    demosDownHolder.classList.add('hidden');
+}
+demosUp.onclick = function () {
+    demosUpHolder.classList.toggle('hidden');
+    albumHolder.classList.add('hidden');
+    demosDownHolder.classList.add('hidden');
+}
+demosDown.onclick = function () {
+    demosDownHolder.classList.toggle('hidden');
+    demosUpHolder.classList.add('hidden');
+    albumHolder.classList.add('hidden');
+}
