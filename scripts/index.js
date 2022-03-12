aaff690296052b5b97dd2ac8269599008598e328
Zale Davis <zale.davis@gmx.com>
2022-03-11 18:03:35 -0800

    add background videos to welcome page

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const color = document.querySelector('.doorColor');
+const plantVid = document.getElementById('thistleVideo');
+const cloudVid = document.getElementById('cloudVideo');
+
+function doorColor(a) {
+    color.style.backgroundColor = a;
+}
+
+function plantVideo() {
+    plantVid.classList.toggle('hidden');
+    if (!cloudVid.classList.contains('hidden')) {
+        cloudVid.classList.add('hidden');
+    }
+}
+function cloudVideo() {
+    cloudVid.classList.toggle('hidden');
+    if (!plantVid.classList.contains('hidden')) {
+        plantVid.classList.add('hidden');
+    }
+}
