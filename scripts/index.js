a0b363f0bc42690df1fdc2cff9963c81fee215b6
Zale Davis <zale.davis@gmx.com>
2022-03-13 14:57:39 -0700

    added responsive design to welcome page, added collapsable navbar

--- scripts/index.js
+++ scripts/index.js
@@ -18,3 +18,21 @@ function cloudVideo() {
         plantVid.classList.add('hidden');
     }
 }
+
+function showmenu() {
+    var x = document.getElementById('myUL');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function openNav() {
+    let nav = document.getElementById("navBar");
+    if (nav.className === "navigation") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "navigation";
+    }
+}
